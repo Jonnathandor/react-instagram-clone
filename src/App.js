3e0d94c1a91a3ee3735b5d17b4dc3e716755daee
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-08-08 18:11:55 -0500

    added Post layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
     <div className="App">
       <div className="app__header">
-
+        <img className="app__headerImage"
+        src="https://cdn.pixabay.com/photo/2017/03/24/07/28/instagram-2170420_960_720.png" />
       </div>
+      <Post
+      username="Jonnathan"
+      caption="elephant"
+      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
+            <Post
+      username="Jonnathan"
+      caption="elephant"
+      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
+            <Post
+      username="Jonnathan"
+      caption="elephant"
+      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
+            <Post
+      username="Jonnathan"
+      caption="elephant"
+      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
     </div>
   );
 }
