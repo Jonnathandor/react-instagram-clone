21bb1beaf5e7da96a22d3c41657a1ff02619e37d
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-08-08 19:01:04 -0500

    fetching data from firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Post from './Post';
+import { db } from './firebase';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, [])
+
   return (
     <div className="App">
       <div className="app__header">
         <img className="app__headerImage"
-        src="https://cdn.pixabay.com/photo/2017/03/24/07/28/instagram-2170420_960_720.png" />
+        src="https://cdn.pixabay.com/photo/2017/03/24/07/28/instagram-2170420_960_720.png"
+        alt="" />
       </div>
-      <Post
-      username="Jonnathan"
-      caption="elephant"
-      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
-            <Post
-      username="Jonnathan"
-      caption="elephant"
-      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
-            <Post
-      username="Jonnathan"
-      caption="elephant"
-      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
-            <Post
-      username="Jonnathan"
-      caption="elephant"
-      imageUrl="https://cdn.pixabay.com/photo/2018/11/22/18/17/elephant-3832516_960_720.jpg"/>
+      {posts.map(({ id, post }) => (
+        <Post 
+        key={id}
+        username={post.username} 
+        caption={post.caption}
+        imageUrl={post.imageUrl}/>
+      ))}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -11,9 +11,9 @@ function Post({username, caption, imageUrl }) {
                 alt=""
                 src=""
                 />
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
-            <img className="post__image" src={imageUrl}/>
+            <img className="post__image" src={imageUrl} alt=""/>
             <h4 className="post__text"><strong>{ username }</strong> { caption }</h4>
         </div>
     )
